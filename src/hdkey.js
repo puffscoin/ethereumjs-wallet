96d844d1b55f7443dff8e868c50d45391f6a3203
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-05-21 21:55:18 -0300

    Update hdkey.js

--- src/hdkey.js
+++ src/hdkey.js
@@ -1,46 +1,46 @@
 const HDKey = require('hdkey')
 const Wallet = require('./index.js')
 
-function EthereumHDKey () {
+function PuffscoinHDKey () {
 }
 
 /*
  * Horrible wrapping.
  */
 function fromHDKey (hdkey) {
-  var ret = new EthereumHDKey()
+  var ret = new PuffscoinHDKey()
   ret._hdkey = hdkey
   return ret
 }
 
-EthereumHDKey.fromMasterSeed = function (seedBuffer) {
+PuffscoinHDKey.fromMasterSeed = function (seedBuffer) {
   return fromHDKey(HDKey.fromMasterSeed(seedBuffer))
 }
 
-EthereumHDKey.fromExtendedKey = function (base58key) {
+PuffscoinHDKey.fromExtendedKey = function (base58key) {
   return fromHDKey(HDKey.fromExtendedKey(base58key))
 }
 
-EthereumHDKey.prototype.privateExtendedKey = function () {
+PuffscoinHDKey.prototype.privateExtendedKey = function () {
   if (!this._hdkey.privateExtendedKey) {
     throw new Error('This is a public key only wallet')
   }
   return this._hdkey.privateExtendedKey
 }
 
-EthereumHDKey.prototype.publicExtendedKey = function () {
+PuffscoinHDKey.prototype.publicExtendedKey = function () {
   return this._hdkey.publicExtendedKey
 }
 
-EthereumHDKey.prototype.derivePath = function (path) {
+PuffscoinHDKey.prototype.derivePath = function (path) {
   return fromHDKey(this._hdkey.derive(path))
 }
 
-EthereumHDKey.prototype.deriveChild = function (index) {
+PuffscoinHDKey.prototype.deriveChild = function (index) {
   return fromHDKey(this._hdkey.deriveChild(index))
 }
 
-EthereumHDKey.prototype.getWallet = function () {
+PuffscoinHDKey.prototype.getWallet = function () {
   if (this._hdkey._privateKey) {
     return Wallet.fromPrivateKey(this._hdkey._privateKey)
   } else {
@@ -48,4 +48,4 @@ EthereumHDKey.prototype.getWallet = function () {
   }
 }
 
-module.exports = EthereumHDKey
+module.exports = PuffscoinHDKey
